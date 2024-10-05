d6d9dc3ed571af5ddc45c006a95ffc9e0c6349f0
shade477 <sagardeyof477@gmail.com>
2024-10-05 12:37:33 +0530

    added character count function

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ def count_words(text):
     word_count = len(text.split())
     return word_count
 
+def char_count(strings):
+    count = {}
+    for c in strings:
+        c.lower()
+        if ord('a') <= ord(c) <= ord('z'):
+            count[c] = 1 + count.get(c, 0)  
+    return count
+
+def display(obj):
+    print(obj)
+
 if __name__ == '__main__':
     path = 'books/frankenstein.txt'
     text = book_process(path)
     word_count = count_words(text)
+    display(word_count)
+    chars = char_count(text)
+    display(chars)
